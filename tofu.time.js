68e47cda9b87f990991a90cbd0948c1051debb41
Michael Laszlo <git@michaellaszlo.com>
2016-01-27 23:57:54 -0500

    Show message on load

--- tofu.time.js
+++ tofu.time.js
@@ -1,5 +1,14 @@
 var TofuTime = (function () {
+
+  function addMessage(s) {
+    document.getElementById('messageBox').innerHTML += s + '<br>';
+  }
+  function setMessage(s) {
+    document.getElementById('messageBox').innerHTML = s + '<br>';
+  }
+
   function load() {
+    addMessage('Ready.');
   }
   return {
     load: load
