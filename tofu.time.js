f2b30a9b398bdf9dc018653bfcd783926bcc519d
Michael Laszlo <git@michaellaszlo.com>
2016-01-28 23:59:01 -0500

    Level map with platforms and ladders

--- tofu.time.js
+++ tofu.time.js
@@ -1,4 +1,18 @@
 var TofuTime = (function () {
+  var levels = [
+    {
+      map: [
+        '   xxxxxx            ',
+        '   |  |              ',
+        '   |  xxxxxxx        ',
+        '   |       |         ',
+        '  xxxx   xxxxxxxx    ',
+        '          |          ',
+        '    xxxxxxxx         ',
+        '                     '
+      ]
+    }
+  ];
 
   function addMessage(s) {
     document.getElementById('messageBox').innerHTML += s + '<br>';
@@ -7,8 +21,16 @@ var TofuTime = (function () {
     document.getElementById('messageBox').innerHTML = s + '<br>';
   }
 
+  function loadLevel(levelIndex) {
+    var level = levels[levelIndex],
+        map = level.map,
+        numRows = map.length,
+        numCols = map[0].length;
+  }
+
   function load() {
-    addMessage('Ready.');
+    loadLevel(0);
+    addMessage('Loaded.');
   }
   return {
     load: load
