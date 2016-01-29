497324fd5d4bf1e34805adf7e186322b393b77a4
Michael Laszlo <git@michaellaszlo.com>
2016-01-29 02:13:48 -0500

    Paint platforms

--- tofu.time.js
+++ tofu.time.js
@@ -3,15 +3,21 @@ var TofuTime = (function () {
         {
           map: [
             '                     ',
+            '                     ',
             ' xxxxxxxxxxxxxxxxxxx ',
+            ' |    |      |     | ',
             ' |    |      |     | ',
             ' |    xxxxxxxx     | ',
             ' |         |       | ',
+            ' |         |       | ',
             ' xxxx    xxxxxxxx  | ',
+            '    |     |     |  | ',
             '    |     |     |  | ',
             '    xxxxxxx     |  | ',
             '    |           |  | ',
+            '    |           |  | ',
             ' xxxxxxx      xxxxxx ',
+            ' |     |      |    | ',
             ' |     |      |    | ',
             ' xxxxxxxxxxxxxxxxxxx '
           ]
@@ -20,7 +26,12 @@ var TofuTime = (function () {
       currentLevel,
       size = {
         cell: 32,
+        level: {},
         container: { border: 4 }
+      },
+      color = {
+        background: '#e0d9a9',
+        platform: { stroke: '#2b3648', fill: '#48576f' }
       },
       container = {},
       canvas = {},
@@ -76,6 +87,27 @@ var TofuTime = (function () {
   }
 
   function paintLevel() {
+    var level = currentLevel,
+        platforms = level.platforms,
+        i, platform,
+        x0, y0, xSize, ySize,
+        layer = context.furniture;
+    layer.fillStyle = color.background;
+    layer.fillRect(0, 0, size.level.width, size.level.height);
+    layer.fillStyle = color.platform.fill;
+    layer.strokeStyle = color.platform.stroke;
+    layer.lineWidth = 1;
+    for (i = 0; i < platforms.length; ++i) {
+      platform = platforms[i];
+      console.log('platform r', platform.rMin, 'c', platform.cMin,
+          'length', platform.cMax - platform.cMin + 1);
+      x0 = platform.cMin * size.cell;
+      y0 = platform.rMin * size.cell;
+      xSize = (platform.cMax - platform.cMin + 1) * size.cell;
+      ySize = (platform.rMax - platform.rMin + 1) * size.cell;
+      layer.fillRect(x0, y0, xSize, ySize);
+      layer.strokeRect(x0, y0, xSize, ySize);
+    }
   }
 
   function loadLevel(levelIndex) {
@@ -83,13 +115,13 @@ var TofuTime = (function () {
         map = level.map,
         numRows = level.numRows = map.length,
         numCols = level.numCols = map[0].length,
-        physicalWidth = numCols * size.cell,
-        physicalHeight = numRows * size.cell,
-        platforms = [],
-        ladders = [],
+        platforms = level.platforms = [],
+        ladders = level.ladders = [],
         flooded = new Array(numRows),
         thing,
         r, c, ch;
+    size.level.width = numCols * size.cell;
+    size.level.height = numRows * size.cell;
     for (r = 0; r < numRows; ++r) {
       flooded[r] = new Array(numCols);
     }
@@ -102,21 +134,20 @@ var TofuTime = (function () {
         if (ch == 'x' || ch == '|') {
           thing = scanThingOnMap(flooded, ch, r, c);
           if (ch == 'x') {
-            console.log('platform', 'r', thing.rMin, 'c', thing.cMin,
-                'length', thing.cMax - thing.cMin + 1);
-          } else {
+            platforms.push(thing);
           }
         }
       }
     }
-    container.game.style.width = physicalWidth +
+    container.game.style.width = size.level.width +
         2 * size.container.border + 'px';
-    container.game.style.height = physicalHeight +
+    container.game.style.height = size.level.height +
         2 * size.container.border + 'px';
     Object.keys(canvas).forEach(function (name) {
-      canvas[name].width = physicalWidth;
-      canvas[name].height = physicalHeight;
+      canvas[name].width = size.level.width;
+      canvas[name].height = size.level.height;
     });
+    paintLevel();
   }
 
   function load() {
