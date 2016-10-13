27cac2906a3ed24bd3e0dd24595c19568b363358
Michael Laszlo <git@michaellaszlo.com>
2016-10-12 23:59:27 -0400

    Write slab and tower constructors

--- sandwich_time.js
+++ sandwich_time.js
@@ -1,13 +1,27 @@
 var SandwichTime = (function () {
 
-  function addMessage(s) {
-    document.getElementById('messageBox').innerHTML += s + '<br>';
+  function Slab(width, height, upperLeft, paint) {
+    var width, height, upperLeft, paint;
+    this.width = width;
+    this.height = height;
+    this.upperLeft = upperLeft;
+    this.paint = paint;
   }
-  function setMessage(s) {
-    document.getElementById('messageBox').innerHTML = s + '<br>';
+
+  function Tower(slabs) {
+    var numSlabs,
+        slabs;
+    this.numSlabs = slabs.length;
+    this.slabs = slabs;
   }
 
   function load() {
+    var slabs = [
+          new Slab(5, 2, { x: 10, y: 5 }, null),
+          new Slab(5, 2, { x: 10, y: 10 }, null)
+        ],
+        tower = new Tower(slabs);
+    console.log(JSON.stringify(tower.slabs));
   }
 
   return {
